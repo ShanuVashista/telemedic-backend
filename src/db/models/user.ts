736ad5ba000a8029154a76c77e42c6dfb93ab446
manishprimelab <manish.saraswat@primelab.io>
2022-04-19 00:42:13 +0530

    modify user schema

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -1,11 +1,37 @@
 import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema (
+const userSchema = new mongoose.Schema(
     {
-        email: {type: String, unique: true},
-        image: String,
-        password: String,
-        type:{ type: String, enum: ["patient", "doctor"], required: true }
+        email: { type: String, unique: true },
+
+        profile_photo: { type: String },
+
+        password: { type: String },
+
+        role_id: { type: String, enum: ["patient", "doctor"], required: true },
+
+        firstname: { type: String }, 
+
+        lastname: { type: String },
+
+        gender: { type: String }, 
+
+        dob: { type: String },
+
+        phone: { type: Number },
+
+        fax: { type: Number },
+
+        specialty: { type: String }, 
+
+        qualification: { type: String }, 
+
+        total_exp: { type: String }, 
+
+        current_practise_address: { type: Object }, 
+
+        license: { type: Array }
+
     },
     {
         timestamps: true
