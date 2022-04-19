4a9ff6f06d84f6c20c2fe783ee63833350b0c9cf
ashish primelab <ashish.singla@primelab.io>
2022-04-19 14:48:05 +0530

    refactor db connection code

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import express from 'express';
 import bodyparser from 'body-parser'
 import cors from 'cors'
 import path from "path";
+import { config } from 'dotenv';
 import getConnection from './db/connection'
+config();
 const app = express()
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }))
@@ -12,10 +14,7 @@ app.use(cors());
 app.use(express.static('public'))
 
 //DATABASE CONNECTION
-app.use((req, res, next) => {
-    getConnection(req, res)
-    next()
-})
+app.use(getConnection)
 app.use("/public", express.static(path.join(__dirname, "public")));
 //ROUTES
 import userRoutes from './routes/userRoute'
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,20 +1,21 @@
+import { StatusCodes } from 'http-status-codes';
 import mongoose from 'mongoose';
-const getConnection = async (req, res) => {
+const getConnection = async (req, res, next) => {
 
     // This is mongoDB ATLAS URI So you can use this same for your test.
-    // Uri = mongodb+srv://pratyush:sawan123@cluster0.36efv.mongodb.net/teleMd?retryWrites=true&w=majority
-    const dbUri =
-    "mongodb+srv://pratyush:sawan123@cluster0.36efv.mongodb.net/teleMd?retryWrites=true&w=majority";
+    const dbUri = process.env.database_uri;
 
     try {
         await mongoose.connect(dbUri)
         console.log('Database Connected to the MongoDB')
+        next();
     } catch (error) {
-        return res.status(400).json({
+        console.log('Error in connecting to the MongoDB', error)
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             message: 'Failed in Database Connection',
             status: false,
             error: error
-        })
+        }).end()
     }
 }
 export default getConnection
