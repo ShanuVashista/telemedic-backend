57ab674dd2c21b7551e4796629c8e8791c41471b
ashish primelab <ilokenathkarmakar@gmail.com>
2022-04-21 14:39:24 +0530

    list notification

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,9 +4,11 @@ import List_POST from '../controllers/user/list';
 import patientRouter from './patient.route';
 import doctorRouter from './doctor.route';
 import auth from '../middlewares/auth.middleware';
+import notificationRouter from './notification.route';
 const router = express.Router()
 router.use('/patient', patientRouter);
 router.use('/doctor', doctorRouter);
 router.post("/login", Login.login);
 router.post('/list', auth, List_POST)
+router.use('/notifications', auth, notificationRouter)
 export default router
