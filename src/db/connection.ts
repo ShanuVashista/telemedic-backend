ad70e03ebcf0f7d8239ec09402c7a673b2e82c45
ashish primelab <ilokenathkarmakar@gmail.com>
2022-04-21 13:00:09 +0530

    only use DATABASE_URI from env

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,12 +1,12 @@
 import { StatusCodes } from 'http-status-codes';
 import mongoose from 'mongoose';
 const getConnection = async (req, res, next) => {
-
-    // This is mongoDB ATLAS URI So you can use this same for your test.
-    const dbUri = "mongodb://SohamDB:Pa$$w(2)Rd22@localhost:27017/thera_net?authSource=admin" ;
+    if (!process.env.DATABASE_URI) {
+        throw new Error('Database URI not found');
+    }
 
     try {
-        await mongoose.connect(dbUri ?? process.env.database_uri)
+        await mongoose.connect(process.env.DATABASE_URI)
         console.log('Database Connected to the MongoDB')
         next();
     } catch (error) {
