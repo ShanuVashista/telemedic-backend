d470507d6c7c6bb7462fee5316e92b99c3fff883
ashish primelab <ilokenathkarmakar@gmail.com>
2022-04-21 13:27:48 +0530

    refactor joi schema for query validation

---
+++ src/validator/healthProfile.ts
@@ -0,0 +1,20 @@
+import Joi from "joi";
+import { paginationQuerySchema } from "./util";
+
+export const healthProfileQuerySchema = paginationQuerySchema.keys({
+    f: Joi.object().keys({
+        name: Joi.string(),
+        weight: Joi.number(),
+        height: Joi.number(),
+        bmi: Joi.number(),
+        medicalCondition: Joi.string(),
+        pastMedicalCondition: Joi.string(),
+        alergies: Joi.string(),
+        medication: Joi.string(),
+        smoking: Joi.boolean(),
+        alcohol: Joi.boolean(),
+        marijuana: Joi.boolean(),
+    }).default({}).options({
+        allowUnknown: false,
+    })
+});
