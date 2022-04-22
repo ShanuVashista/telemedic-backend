6bfe2a497975e24c80e9e8ea39509b604140f7c7
ashish primelab <ilokenathkarmakar@gmail.com>
2022-04-22 15:45:54 +0530

    remove old image on updating new image

--- src/controllers/patient/healthProfile/updateHealthProfile.controller.ts
+++ src/controllers/patient/healthProfile/updateHealthProfile.controller.ts
@@ -1,14 +1,17 @@
 import { StatusCodes } from 'http-status-codes';
-import HealthProfile from '../../../db/models/healthProfile.model';
+import mongoose from 'mongoose';
+import path from 'path';
+import HealthProfile, { IHealthProfile } from '../../../db/models/healthProfile.model';
 import { deleteFileByPath } from '../../../lib/deleteFileByPath';
 import { saveFile } from '../../../lib/saveFile';
 
 export const updateHealthProfile = async (req, res) => {
     try {
-        const healthProfile = await HealthProfile.findOneAndUpdate({
-            _id: req.params.id,
-            userId: req.user._id,
-        }, { ...req.body, profile_image: req.file?.filename }, { new: true });
+        const healthProfile: mongoose.HydratedDocument<IHealthProfile> =
+            await HealthProfile.findOne({
+                userId: req.user._id,
+                _id: req.params.id,
+            })
 
         if (!healthProfile) {
             return res.status(StatusCodes.NOT_FOUND).json({
@@ -16,7 +19,23 @@ export const updateHealthProfile = async (req, res) => {
             });
         }
 
+        const oldProfileImage = healthProfile.profile_image;
+
+        healthProfile.profile_image = req.file?.filename;
+        Object.entries(req.body).forEach(([key, value]) => {
+            healthProfile[key] = value;
+        });
+
+        await healthProfile.save();
+
         await saveFile(req.user, req);
+
+        if (oldProfileImage) {
+            const userDir = path.resolve(`./public/uploads/${req.user._id}`);
+            const oldFilePath = path.join(userDir, oldProfileImage);
+
+            await deleteFileByPath(oldFilePath);
+        }
 
         return res.status(StatusCodes.OK).json({
             message: "Health data updated",
