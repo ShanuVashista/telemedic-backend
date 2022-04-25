8ec9687ba9d6309f9cba0257f8b8f9029861a06b
Pratyush Raj <pratyushrajsingh22@gmail.com>
2022-04-25 18:47:03 +0530

    changed userId name to patientId

--- src/db/models/appointment.model.ts
+++ src/db/models/appointment.model.ts
@@ -3,9 +3,9 @@ import { Schema, model} from "mongoose";
 const AppointmentSchema: Schema = new Schema(
   {
     appointmentId: { type: Schema.Types.ObjectId },
-    userId: { type: String, required: true },
+    patientId: { type: Schema.Types.ObjectId, required: true },
     // doctorId: { type: Number, required: true },
-    doctor: {type: Schema.Types.ObjectId, ref:'users'},
+    doctorId: {type: Schema.Types.ObjectId, ref:'users'},
     appointmentType: { type: String, required: true },
     dateOfAppointment: { type: Date, required: true },
     status: { default: "Pending", type: String, required: true },
