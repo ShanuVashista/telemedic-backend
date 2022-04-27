2aadd2ab9e2342ef9ac7f4264c7f1e31b27cfd3b
shubhammandloi82 <shubhammandloi82@gmail.com>
2022-04-27 21:20:58 +0530

    change in user list api add check for only admin use

--- src/controllers/user/list.ts
+++ src/controllers/user/list.ts
@@ -5,35 +5,45 @@ import StatusCodes from "http-status-codes";
 import User from '../../db/models/user';
 const List_POST = async (req, res) => {
     try {
-        let { page, limit, sort, cond } = req.body;
-        if (!page || page < 1) {
-            page = 1;
+        if(req.user.role_id == 'admin'){
+            let { page, limit, sort, cond } = req.body;
+            if (!page || page < 1) {
+                page = 1;
+            }
+            if (!limit) {
+                limit = 10;
+            }
+            if (!cond) {
+                cond = {}
+            }
+            if (!sort) {
+                sort = { "createdAt": -1 }
+            }
+            limit = parseInt(limit);
+            const user = await User.find(cond).sort(sort).skip((page - 1) * limit).limit(limit)
+            user.forEach(oneUser => oneUser.populate('paymentMethods'))
+    
+            const user_count = await User.find(cond).count()
+            const totalPages = Math.ceil(user_count / limit);
+            res.status(StatusCodes.OK).send({
+                status:true,
+                type: 'success',
+                message: "User List Fetch Successfully",
+                page: page,
+                limit: limit,
+                totalPages: totalPages,
+                total: user_count,
+                data: user,
+            });
+        }else{
+            res.status(400).send({
+                status:false,
+                type: 'error',
+                message: "You Are Not Authorized User"
+            });
         }
-        if (!limit) {
-            limit = 10;
-        }
-        if (!cond) {
-            cond = {}
-        }
-        if (!sort) {
-            sort = { "createdAt": -1 }
-        }
-        limit = parseInt(limit);
-        const user = await User.find(cond).sort(sort).skip((page - 1) * limit).limit(limit)
-        user.forEach(oneUser => oneUser.populate('paymentMethods'))
-
-        const user_count = await User.find(cond).count()
-        const totalPages = Math.ceil(user_count / limit);
-        res.status(StatusCodes.OK).send({
-            status:true,
-            type: 'success',
-            message: "User List Fetch Successfully",
-            page: page,
-            limit: limit,
-            totalPages: totalPages,
-            total: user_count,
-            data: user,
-        });
+        
+        
     } catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json({
             status:false,
