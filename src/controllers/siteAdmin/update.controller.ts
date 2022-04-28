4bd880cf6fd18d8871eebc4a24ef35f84e257466
shiveshcshukla <soham.mitra@brainiuminfotech.com>
2022-04-28 14:48:52 +0530

    implete ejs template and modify update admin

--- src/controllers/siteAdmin/update.controller.ts
+++ src/controllers/siteAdmin/update.controller.ts
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../../db/models/user";
 import { Roles } from "../../lib/roles";
-
+import S3 from '../../services/upload';
 export const updateAdmin = async (req, res) => {
     try {
         const admin = await User.findOne({
@@ -22,12 +22,21 @@ export const updateAdmin = async (req, res) => {
         });
 
         await admin.save();
-
+        let response = {};
+        if(typeof (req.files) != 'undefined' && req.files != null){
+            const upload_data = {
+                db_response: admin,
+                file: req.files[0]
+            }
+            await S3.deleteFile(JSON.parse(JSON.stringify(admin)));
+            const image_uri = await S3.uploadFile(upload_data);
+            response = await User.findByIdAndUpdate(admin._id, { $set: { "profile_photo": image_uri.Location } }, { new: true });
+        }
         return res.status(StatusCodes.OK).json({
             type: "success",
             status: true,
             message: "Admin Updated",
-            data: admin
+            data: response
         });
     } catch (Err) {
         console.log(Err);
