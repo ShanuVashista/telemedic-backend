2308df0add3ea2b78dbfe47a2b5e313e8c7a75d3
ashish primelab <ilokenathkarmakar@gmail.com>
2022-04-28 17:45:21 +0530

    add doctor availability

--- src/middlewares/joi.middleware.ts
+++ src/middlewares/joi.middleware.ts
@@ -1,3 +1,4 @@
+import { StatusCodes } from "http-status-codes";
 import { Schema } from "joi";
 
 
@@ -15,17 +16,30 @@ export const validateParams = (schema: Schema) => {
 
 export const validate = (schema: Schema, key) => {
     return (req, res, next) => {
-        const { error } = schema.validate(req[key]);
-        const valid = error == null;
+        try {
+            const { error } = schema.validate(req[key]);
+            const valid = error == null;
 
-        if (valid) {
-            next();
-        } else {
-            const { details } = error;
-            const message = details.map(i => i.message).join(',');
+            if (valid) {
+                next();
+            } else {
+                const { details } = error;
+                const message = details.map(i => i.message).join(',');
 
-            console.log("error", message);
-            res.status(422).json({ error: message })
+                console.log("error", message);
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    type: 'error',
+                    status: false,
+                    message: message,
+                });
+            }
+        } catch (error) {
+            console.log("error", error);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                type: 'error',
+                status: false,
+                message: error.message,
+            });
         }
     }
 }
