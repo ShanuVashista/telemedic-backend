d72baa5de1c99c30e7d2ee6a10e62f7234906275
shiveshcshukla <soham.mitra@brainiuminfotech.com>
2022-04-29 20:55:33 +0530

    implement state list endpoint

--- src/routes/country_state_city.ts
+++ src/routes/country_state_city.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import controller from "../controllers/country-state-city/insert";
 import GetCountryCode from  '../controllers/country-state-city/getCountryCode'
+import GetState from  '../controllers/country-state-city/getState'
 const router = express.Router();
 router.get("/", controller);
 router.get("/dial-code", GetCountryCode);
+router.get("/states", GetState);
 export = router;
