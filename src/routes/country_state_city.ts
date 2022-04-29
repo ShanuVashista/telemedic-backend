f790dea4679f2f7972cf0138da2ed5569c227e39
shiveshcshukla <soham.mitra@brainiuminfotech.com>
2022-04-29 20:33:53 +0530

    implement get country code list endpoint

--- src/routes/country_state_city.ts
+++ src/routes/country_state_city.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import controller from "../controllers/country-state-city/insert";
+import GetCountryCode from  '../controllers/country-state-city/getCountryCode'
 const router = express.Router();
 router.get("/", controller);
+router.get("/dial-code", GetCountryCode);
 export = router;
