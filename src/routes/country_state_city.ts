edcc85a9e405734dfa915568215f4b75e93bde19
Pratyush Raj <pratyushrajsingh22@gmail.com>
2022-04-29 20:43:13 +0530

    country list from serverside

--- src/routes/country_state_city.ts
+++ src/routes/country_state_city.ts
@@ -1,5 +1,7 @@
 import express from "express";
-import controller from "../controllers/country-state-city/insert";
+// import controller from "../controllers/country-state-city/insert";
+import getCountry from "../controllers/country-state-city/getCountry";
 const router = express.Router();
-router.get("/", controller);
+// router.get("/", controller);
+router.get("/countryName",getCountry);
 export = router;
