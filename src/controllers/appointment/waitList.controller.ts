f04eee42d63c71f90a4603d5fa6a5c5ac1681acc
ashish primelab <ilokenathkarmakar@gmail.com>
2022-05-10 23:42:36 +0530

    fix waitlist

--- src/controllers/appointment/waitList.controller.ts
+++ src/controllers/appointment/waitList.controller.ts
@@ -1,48 +1,36 @@
-import { StatusCodes } from "http-status-codes";
-import { isAfter, differenceInMinutes } from "date-fns";
-import Appointment from "../../db/models/appointment.model";
-import { AppointmentStatuses } from "../../lib/appointmentStatuses";
-import { getSlotIndex, getSlots } from "../../lib/utils/timeSlots";
-import Availability from "../../db/models/availability.model";
-import { generateAvailabilityByTimeFilter } from "./availabilityUtil";
+import { StatusCodes } from 'http-status-codes';
+import { differenceInMinutes, subMinutes } from 'date-fns';
+import Appointment from '../../db/models/appointment.model';
+import { AppointmentStatuses } from '../../lib/appointmentStatuses';
+import { getSlotIndex, getSlots } from '../../lib/utils/timeSlots';
+import Availability from '../../db/models/availability.model';
+import { generateAvailabilityByTimeFilter } from './availabilityUtil';
+import { MIN_MEETING_DURATION } from '../../../constant';
 
 export const waitList = async (req, res) => {
   try {
     const appointmentData = await Appointment.find({
       patientId: req.user._id,
+      status: AppointmentStatuses.APPROVED,
       dateOfAppointment: {
-        $gte: new Date(),
-      },
-    }).limit(1);
+        $gte: subMinutes(new Date(), MIN_MEETING_DURATION),
+      }
+    })
+      .sort({ dateOfAppointment: 1 })
+      .limit(1);
+
+    if (appointmentData.length === 0) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        type: 'error',
+        status: false,
+        message: 'No appointment found',
+      });
+    }
 
     const appointment = await Appointment.findOne({
       patientId: req.user._id,
       _id: appointmentData[0]._id,
     });
-
-    if (!appointment) {
-      return res.status(StatusCodes.NOT_FOUND).json({
-        type: "error",
-        status: false,
-        message: "Appointment not found",
-      });
-    }
-
-    if (appointment.status !== AppointmentStatuses.PENDING) {
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        type: "error",
-        status: false,
-        message: "Appointment is not pending",
-      });
-    }
-
-    if (isAfter(new Date(), appointment.dateOfAppointment)) {
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        type: "error",
-        status: false,
-        message: "Appointment date is in the past",
-      });
-    }
 
     appointment.dateOfAppointment.setSeconds(0, 0);
 
@@ -53,9 +41,9 @@ export const waitList = async (req, res) => {
 
     if (availability === null) {
       return res.status(StatusCodes.OK).json({
-        type: "success",
+        type: 'success',
         status: true,
-        message: "Doctor Not Available At booked Time Schedule",
+        message: 'Doctor Not Available At booked Time Schedule',
       });
     }
 
@@ -78,12 +66,12 @@ export const waitList = async (req, res) => {
     }).sort({ dateOfAppointment: 1 });
 
     // filter out completed appointments
-    const filteredAppointments = appointments.filter(
-      (appointment) => appointment.status !== AppointmentStatuses.COMPLETED
+    const precedingAppointments = appointments.filter(
+      (appointment) => appointment.status === AppointmentStatuses.APPROVED
     );
 
     // find in progress appointment
-    const inProgressAppointment = filteredAppointments.find(
+    const inProgressAppointment = precedingAppointments.find(
       (appointment) => appointment.status === AppointmentStatuses.IN_PROGRESS
     );
 
@@ -91,21 +79,25 @@ export const waitList = async (req, res) => {
       slots,
       inProgressAppointment?.dateOfAppointment
     );
-
     const slotIndexOfAppointment = getSlotIndex(
       slots,
       appointment.dateOfAppointment
     );
 
+    const precidingAppointmentCompletionTime =
+      precedingAppointments.length * 30;
     // calculate estimated wait time
     const estimatedWaitTime = inProgressAppointment
-      ? filteredAppointments.length * 30
-      : differenceInMinutes(appointment.dateOfAppointment, new Date());
+      ? precidingAppointmentCompletionTime + MIN_MEETING_DURATION
+      : Math.max(
+        0,
+        differenceInMinutes(appointment.dateOfAppointment, new Date())
+      );
 
     return res.status(StatusCodes.OK).json({
-      type: "success",
+      type: 'success',
       status: true,
-      message: "Appointment found",
+      message: 'Appointment found',
       data: { appointment },
       est: estimatedWaitTime,
       inProgressAppointment: inProgressAppointment
@@ -116,7 +108,7 @@ export const waitList = async (req, res) => {
   } catch (error) {
     console.log({ error });
     return res.status(400).json({
-      type: "error",
+      type: 'error',
       status: false,
       message: error.message,
     });
