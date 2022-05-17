a2b35ccac7bb85762612c438cf8e0bf9eee9584f
ashish primelab <ilokenathkarmakar@gmail.com>
2022-05-17 14:53:39 +0530

    default value as enum

--- src/db/models/appointment.model.ts
+++ src/db/models/appointment.model.ts
@@ -1,10 +1,11 @@
 import { Schema, model, PopulatedDoc } from "mongoose";
+import { AppointmentStatuses } from "../../lib/appointmentStatuses";
 import { IHealthProfile } from "./healthProfile.model";
 import { IUser } from "./user";
 
 export interface IAppointment {
   appointmentId: Schema.Types.ObjectId;
-  userId:PopulatedDoc<IUser>;
+  userId: PopulatedDoc<IUser>;
   patientId: PopulatedDoc<IHealthProfile>;
   doctorId: PopulatedDoc<IUser>;
   appointmentType: string;
@@ -12,23 +13,23 @@ export interface IAppointment {
   status: string;
   isEmergency: boolean;
   symptoms: Array<string>;
-  reason:string,
+  reason: string,
   Meta: string;
 }
 
 const AppointmentSchema = new Schema<IAppointment>(
   {
     appointmentId: { type: Schema.Types.ObjectId },
-    userId:{type: Schema.Types.ObjectId,ref:'user',required:true},
+    userId: { type: Schema.Types.ObjectId, ref: 'user', required: true },
     patientId: { type: Schema.Types.ObjectId, ref: 'healthProfile', required: true },
     doctorId: { type: Schema.Types.ObjectId, ref: 'user' },
     appointmentType: { type: String, required: true },
     dateOfAppointment: { type: Date, required: true },
-    status: { default: "Pending", type: String, required: true },
+    status: { default: AppointmentStatuses.PENDING, type: String, required: true },
     isEmergency: { default: false, type: Boolean, required: true },
-    symptoms:[{type: String, required: true, default:null}],
+    symptoms: [{ type: String, required: true, default: null }],
     Meta: { type: String },
-    reason:{type:String}
+    reason: { type: String }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
